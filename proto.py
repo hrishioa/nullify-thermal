c305a21df3469043fb471e40ff7a181696b7295a
Hrishi Olickel <hrishioa@gmail.com>
2015-07-03 02:48:18 +0800

    Graph, video and statistics engine complete and working

--- proto.py
+++ proto.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 import sys
+from subprocess import call
+
 print(sys.version)
 
 np.set_printoptions(threshold=np.inf)
@@ -67,15 +69,23 @@ def save(path, ext='png', close=True, verbose=True):
 def main():
 	print("Program Running...")
 
-	video = cv2.VideoCapture('MOV_2146.mp4')
+	filename = 'MOV_2150.mp4'
+
+	video = cv2.VideoCapture('Video/'+filename)
 	print(video.grab())
 
 	counter = 0
 	
+	data = []
+
 	while(video.isOpened()):
+
 		counter+=1
 
 		ret, frame = video.read()
+
+		if ret==False:
+			break
 
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -83,20 +93,34 @@ def main():
 		
 		framedat = gray.ravel()
 
-		plt.hist(framedat,256,[0,256])
-
 		#Process the incoming data
 
 		#find out the limit of valid data
 		pointer = int((hist_samples/(temp_max-temp_min))*(threshold_temp-temp_min))
 
-		data = framedat[pointer:]
+		temp = np.histogram(framedat,bins=256,range=(0,256))[0][pointer:]
 
-		data = np.histogram(framedat,bins=256,range=(0,256))[0][pointer:]
+		delta = [0]
+		if(data!=[]):
+			delta=temp-data
+		data = temp
+
+		firecon=0
+		maxmean = (np.max(data)-np.mean(data))
+
+		if(maxmean>520):
+			firecon=1
+		if(maxmean>600):
+			firecon=2
+		if(maxmean>650):
+			firecon=3
+		if(maxmean>700):
+			firecon=4
 
 		#Plot statistics for now
-		stats = "Pointer: %d \nMean : %.2f \nMedian: %d \nMin : %d \nMax : %d \nSD: %.2f \nMax-Mean: %d" % (pointer,np.mean(data),np.median(data),np.min(data),np.max(data),np.std(data),(np.max(data)-np.mean(data)))
+		stats = "Pointer: %d \nMean : %.2f \nMedian: %d \nMin : %d \nMax : %d \nSD: %.2f \nMax-Mean: %d\n\nFire Alert:      %d" % (pointer,np.mean(data),np.median(data),np.min(data),np.max(data),np.std(data),maxmean,(firecon))
 
+		plt.hist(framedat,256,[pointer,256])
 		plt.annotate(stats, xy=(1, 1), xycoords='axes fraction', fontsize=16,horizontalalignment='right', multialignment='left', verticalalignment='top',bbox=dict(facecolor='black', alpha=0.1))
 
 		''' For plotting a color histogram
@@ -107,11 +131,29 @@ def main():
 			plt.xlim([0,256])
 			'''
 		#plt.show()
-		save("Histograms/MOV_2146_2/histogram#"+str(counter), ext="png", close=True, verbose=False)
-		print("Frame "+str(counter)+" done.")
+		save("Histograms/"+filename+"/histogram#"+str(counter), ext="png", close=True, verbose=False)
+		print(filename+": Frame "+str(counter)+" done.")
+
+		#Now save delta data
+		plt.plot(np.arange(len(delta)),delta)
+		plt.xlabel('Temperature')
+		plt.ylabel('Delta')
+		plt.ylim([-50,50])
+		delstats = "Pointer: %d \nMean : %.2f \nMedian: %d \nMin : %d \nMax : %d \nSD: %.2f \nMax-Mean: %d" % (pointer,np.mean(delta),np.median(delta),np.min(delta),np.max(delta),np.std(delta),(np.max(delta)-np.mean(delta)))
+		plt.annotate(delstats, xy=(1, 1), xycoords='axes fraction', fontsize=16,horizontalalignment='right', multialignment='left', verticalalignment='top',bbox=dict(facecolor='black', alpha=0.1))
+		#plt.show()
+		save("Histograms/"+filename+"/del_histogram#"+str(counter), ext="png", close=True, verbose=False)
+
 
 	video.release()
 	cv2.destroyAllWindows()
+
+	#Run ffmpeg to create video
+	print("Generating video")
+	call("ffmpeg -framerate 30 -y -i del_histogram#%%d.png del_hist"+filename,shell=True)
+	call("ffmpeg -framerate 30 -y -i histogram#%%d.png hist"+filename,shell=True)
+
+
 
 
 if __name__ == '__main__':
